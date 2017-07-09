d25538ce382c2ad70b8a23c07db86906238dd42c
Pranav1895 <pranav_arora@srmuniv.edu.in>
2017-07-09 16:10:57 +0530

    Add files via upload

---
+++ ques1.java
@@ -0,0 +1,40 @@
+    import java.util.*;
+    import java.io.*;
+    public class HelloWorld
+    {
+        public static void main(String[] args) 
+        {
+            int n;
+            Scanner s = new Scanner(System.in);
+
+            System.out.print("Enter the number you want to check:");
+
+            n = s.nextInt();
+
+            if(n > 0)
+
+            {
+
+                System.out.println("The given number "+n+" is Positive");
+
+            }
+
+            else if(n < 0)
+
+            {
+
+                System.out.println("The given number "+n+" is Negative");
+
+            }
+
+            else
+
+            {
+
+                System.out.println("The given number "+n+" is zero ");
+
+            }
+
+        }
+
+    }
